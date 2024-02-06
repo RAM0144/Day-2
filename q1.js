1a00b61ceb692c39f1b0ea050298cf0d097f590d
Ram <ram440163@gmail.com>
2024-02-06 20:07:36 +0530

    #

---
+++ q1.js
@@ -0,0 +1,21 @@
+// WINDOW OBJECTS
+
+// The window object is it represents a browser window or frame that 
+// displays the contents of the webpage.
+// Window largest Object in a browser to access the utilities or function
+// specifically to window.
+// window it is the very first object that is loaded in the browser.
+// global objects,functions and variables of javascript are members of 
+// the window object.
+
+
+// DOCUMENT OBJECTS
+
+// DOCUMENT object it represents any HTML document or web page that is 
+// loaded in the browser.
+// Document object is child of the window which represents the html 
+// produced by the browser.
+// Document object is loaded inside the window
+// Document object is the object of window property.
+// dom-we can access the document from a window using the 
+// window.document.
